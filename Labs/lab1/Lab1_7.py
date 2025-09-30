26ad86d875e56765b18c25ade135dace902622b4
idffs <cheatofdarkexp@yahoo.com>
2025-09-30 12:23:59 +1000

    Create Lab1_7.py

---
+++ Labs/lab1/Lab1_7.py
@@ -0,0 +1,18 @@
+a = 64
+s = 8
+q = a + s
+w = a - s
+e = a * s
+r = a ** s
+t = a / s
+y = a // s
+u = a % s
+
+i = a == s
+o = a != s
+p = a > s
+f = a < s
+g = a >= s
+h = a<= s
+print(q, w, e, r, t, y, u)
+print(i, o, p, f, g, h)
