ba86fee9d47b8b0d0e351e774c99b05f57a6c214
idffs <cheatofdarkexp@yahoo.com>
2025-09-30 12:24:02 +1000

    Create Lab1_9.py

---
+++ Labs/lab1/Lab1_9.py
@@ -0,0 +1,13 @@
+a = 'Cъешь еще этих мягких французских булок, да выпей чаю'
+s = a[0:10]
+d = a[10:22]
+f = a[22:34]
+g = a[34:54]
+print(s)
+print(d)
+print(f)
+print(g)
+
+x = str(s) + str(d) + str(f) + str(g)
+print(x)
+
