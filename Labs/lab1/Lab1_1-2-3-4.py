e4814b4c2ed0e380a785a66eb45550bcb68baa7a
idffs <cheatofdarkexp@yahoo.com>
2025-09-30 12:23:39 +1000

    Create Lab1_1-2-3-4.py

---
+++ Labs/lab1/Lab1_1-2-3-4.py
@@ -0,0 +1,21 @@
+p = 6
+n = 3.25
+c = 'si'
+d = True
+name = 'Ignat'
+age = 18
+print(name, age)
+
+q = 342
+w = 56.2
+e = '43'
+r = q + w + int(e)
+print(r)
+
+
+a = 3
+b = 8
+l = (a + 4*b)*(a - 3*b) + a ** 2
+print(l)
+
+
