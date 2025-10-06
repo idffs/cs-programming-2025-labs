56b5201ce88e17d63d910d4231148fe3a6261e87
idffs <235218353+idffs@users.noreply.github.com>
2025-10-07 06:29:46 +1000

    Update lab3_2.py

--- Labs/lab3/lab3_2.py
+++ Labs/lab3/lab3_2.py
@@ -1,8 +1,5 @@
 input_number = input('Число от 1 до 9: ')
 int(input_number)
-if input_number == 1 or 2 or 3 or 4 or 5 or 6 or 7 or 8 or 9:
-    for i in range(1, 10):
-        a = (f'{input_number} * {i} = {int(input_number) * i}')
-        print(a)
-else:
-    print('Неверно заданное число')
+for i in range(1, 10+1):
+    a = (f'{input_number} * {i} = {int(input_number) * i}')
+    print(a)
