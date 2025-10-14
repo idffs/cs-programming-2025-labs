9f6a505ada9745b23cabc1549d5b5703d477bfd4
idffs <235218353+idffs@users.noreply.github.com>
2025-10-14 11:53:00 +1000

    Update lab3_4.py

--- Labs/lab3/lab3_4.py
+++ Labs/lab3/lab3_4.py
@@ -1,5 +1,7 @@
-user_input = input('Write number: ')
-user_input = int(user_input)
-import math
-a = (math.factorial(user_input))
-print(a)
+ipu = input('Write number: ')
+ipu = int(ipu)
+f = 1
+while ipu > 1:
+    f *= ipu
+    ipu -= 1
+print(f)
