ded431247b4e9ec75698b90ec13dd5d563210123
idffs <235218353+idffs@users.noreply.github.com>
2025-10-21 13:11:02 +1000

    Update lab4_2.py

---
+++ Labs/lab4/lab4_2.py
@@ -0,0 +1,12 @@
+user = int(input('Type number of month: '))
+if user == 12 or user == 1 or user == 2:
+    print('Its Winter')
+elif user == 3 or user == 4 or user == 5:
+    print('Its Sprint')
+elif user == 6 or user == 7 or user == 8:
+    print('Its Summer')
+elif user == 9 or user == 10 or user == 11:
+    print('Its Autumn')
+else:
+    print('Not exist month')
+
