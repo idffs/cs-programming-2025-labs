a7bfc93013805ea0efd2061cde2493a6f9c8c7c2
idffs <235218353+idffs@users.noreply.github.com>
2025-10-28 04:26:31 +1000

    Update lab4_3.py

--- Labs/lab4/lab4_3.py
+++ Labs/lab4/lab4_3.py
@@ -1,7 +1,13 @@
-user = int(input('Type dog year old: '))
-if user > 0 and user < 3:
-    print(f'In human year old: {user * 10.5}')
-elif user > 2 and user < 23:
-    print(f'In human year old: {21 + (user - 2) * 4}')
-else:
-    print('Wront type of information')
+try:
+    user = int(input('Type dog year old: '))
+    if user < 1:
+        print('Error: Need more than 1')
+    elif user > 22:
+        print('Error: Too much')
+    else:
+        if user <3:
+            print(f'In human year old: {user * 10.5}')
+        else:
+            print(f'In human year old: {21 + (user - 2) * 4}')
+except ValueError:
+    print('Error: Not number')
