ba69dc9100132246829cff4c2f2243fdce292a42
idffs <235218353+idffs@users.noreply.github.com>
2025-10-28 04:26:49 +1000

    Update lab4_8.py

---
+++ Labs/lab4/lab4_8.py
@@ -0,0 +1,16 @@
+user = int(input('Type your buy cost: '))
+
+if 0 <= user < 1000:
+    print('Your discount: 0%')
+    print(f'Price is {user}')
+elif user >= 1000 and user < 5000:
+    print('Your discount: 5%')
+    print(f'Price is {user * 0.95}')
+elif user >= 5000 and user <10000:
+    print(f'Your discount: 10%')
+    print(f'Price is {user * 0.9}')
+elif user > 10000:
+    print('Your discount: 15%')
+    print(f'Price is {user * 0.85}')
+else:
+    print('What is this suppose to mean?')
