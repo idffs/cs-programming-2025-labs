9c0ec52692a0467a5a8608d16b8d229513d83519
idffs <235218353+idffs@users.noreply.github.com>
2025-11-10 22:04:49 +1000

    Update lab4_1.py

--- Labs/lab4/lab4_1.py
+++ Labs/lab4/lab4_1.py
@@ -1,5 +1,8 @@
-user = int(input('Type temperature in room: '))
-if user >= 20:
-    print('Conditioner is off')
-else:
-    print('Conditioner is on')
+try:    
+    user = int(input('Type temperature in room(example, 25): '))
+    if user >= 20:
+        print('Conditioner is off')
+    else:
+        print('Conditioner is on')
+except ValueError:
+    print('Error: Not number ')
