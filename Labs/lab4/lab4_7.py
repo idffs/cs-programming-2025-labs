99529df1096a4a551505c36d17a797f4895be351
idffs <235218353+idffs@users.noreply.github.com>
2025-11-10 22:05:12 +1000

    Update lab4_7.py

--- Labs/lab4/lab4_7.py
+++ Labs/lab4/lab4_7.py
@@ -1,8 +1,11 @@
-user = input('Type any 3 number separated by space: ')
-q, w, e = map(int, user.split()) 
-if q < w and q < e:
-    print(q)
-elif w < q and w < e:
-    print(w)
-elif e < q and e < w:
-    print(e)
+try:
+    user = input('Type any 3 number separated by space: ')
+    q, w, e = map(int, user.split()) 
+    if q < w and q < e:
+        print(q)
+    elif w < q and w < e:
+        print(w)
+    elif e < q and e < w:
+        print(e)
+except ValueError:
+            print('Error: You need to type any 3 numbers with given conditions')
