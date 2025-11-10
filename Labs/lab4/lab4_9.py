6721a2eb2095a7a51c4cb84356e9c20567191e72
idffs <235218353+idffs@users.noreply.github.com>
2025-11-10 22:04:37 +1000

    Update lab4_9.py

--- Labs/lab4/lab4_9.py
+++ Labs/lab4/lab4_9.py
@@ -1,9 +1,16 @@
-user = int(input('Type what hour is(0-23): '))
-if user in range(0, 6):
-    print('Its night')
-elif user in range(6, 12):
-    print('Its morning')
-elif user in range(12, 18):
-    print('Its daytime')
-elif user in range(18, 24):
-    print('Its evening')
+try:    
+    user = int(input('Type what hour it is(0-23): '))
+    if user in range(0, 6):
+        print('Its night')
+    elif user == 24:
+        print('24:** same as 0:**, so its night')
+    elif user in range(6, 12):
+        print('Its morning')
+    elif user in range(12, 18):
+        print('Its daytime')
+    elif user in range(18, 24):
+        print('Its evening')
+    else:
+        print('Error: In day 24 hours')
+except ValueError:
+    print('Error: Write hour how it in example')
