9de9d7f981560291bcab77e5e7585f0c9565b89d
idffs <235218353+idffs@users.noreply.github.com>
2025-11-17 23:01:26 +1000

    Create lab5_3.py

---
+++ Labs/lab5/lab5_3.py
@@ -0,0 +1,8 @@
+#Found max argument and divide it on length
+list1 = [1, 3, 4, 5, 7, 8, 9, 12, 15, 26, 13]
+l = len(list1)
+print(f'List length - {l}')
+m = max(list1)
+print(f'List max value - {m}')
+res = m / l
+print(res)
