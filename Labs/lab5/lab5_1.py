fd0f5ebd4277b4f60217b7ee97ffad57323df21c
idffs <235218353+idffs@users.noreply.github.com>
2025-11-17 23:01:17 +1000

    Create lab5_1.py

---
+++ Labs/lab5/lab5_1.py
@@ -0,0 +1,9 @@
+#Change 3 to 30 in list
+list1 = []
+for i in range(1, 11):
+    list1.append(i)
+l = len(list1)
+for i in range(l):
+    if list1[i] == 3 in list1:
+        list1[i] = 30
+print(list1)
