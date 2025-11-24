e56a2cb0b702bd808d47df2aa0d6957a5e42b428
idffs <235218353+idffs@users.noreply.github.com>
2025-11-24 23:13:40 +1000

    Update lab4_3.py

--- Labs/lab4/lab4_3.py
+++ Labs/lab4/lab4_3.py
@@ -1,13 +1,14 @@
+print('Works with numeric value from 1 to 22')
 try:
-    user = int(input('Type dog year old: '))
+    user = int(input('Write dog years old: '))
     if user < 1:
-        print('Error: Need more than 1')
+        print('Error: Need more than 0')
     elif user > 22:
         print('Error: Too much')
     else:
         if user <3:
-            print(f'In human year old: {user * 10.5}')
+            print(f'In human years: {user * 10.5}')
         else:
-            print(f'In human year old: {21 + (user - 2) * 4}')
+            print(f'In human years: {21 + (user - 2) * 4}')
 except ValueError:
-    print('Error: Not number')
+    print('Error: Not numeric value')
