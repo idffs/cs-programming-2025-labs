381c6af31f91d681bd7071a0773e05381a90ea1c
idffs <235218353+idffs@users.noreply.github.com>
2025-11-24 23:13:36 +1000

    Update lab4_10.py

--- Labs/lab4/lab4_10.py
+++ Labs/lab4/lab4_10.py
@@ -1,32 +1,24 @@
 list1 = [4, 6, 8, 9, 10]
+list2 = [2, 3, 5, 7]
 try:
     user = int(input('Type any number: '))
-    b = 2
-    c = 0
-    a = int(user ** 0.5)
-    if user == 1:
-        exit('1 - not simple or composite number')
-    elif user == 0:
-        print('0 - not simple or composite number')
+    if user == 1 or user == 0:
+        print(f'{user} - not simple or composite number')
     elif user < 0:
         print('Negative number cant be simple or composite number')
     else:
-        if user > 10:
+        b = 2
+        a = int(user ** 0.5)
+        if user in list1:
+            print(f'{user} - composite number')
+        elif user in list2:
+            print(f'{user} - simple number')
+        elif user > 10:
             while user % b != 0 and b <= a:
                     b += 1
             if user % b == 0:
-                exit(f'{user} - composite number')
+                print(f'{user} - composite number')
             else:
-                exit(f'{user} - simple number')        
-        elif user in list1:
-            exit(f'{user} - composite number')
-        else:
-            exit(f'{user} - simple number')
-           
-
-        if c == 0:
-            print(f'{user} - simple number')
-        else:
-            print(f'{user} - composite number')
+                print(f'{user} - simple number')
 except ValueError:
-    print('Error: not number')
+    print('Error: not numeric value')
