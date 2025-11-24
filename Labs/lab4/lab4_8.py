fe43440c41514e5ee8714da4b5a2fda2cf6b97ce
idffs <235218353+idffs@users.noreply.github.com>
2025-11-24 23:13:51 +1000

    Update lab4_8.py

--- Labs/lab4/lab4_8.py
+++ Labs/lab4/lab4_8.py
@@ -1,16 +1,19 @@
-user = int(input('Type your buy cost: '))
+try:
+    user = int(input('Type your buy cost: '))
 
-if 0 <= user < 1000:
-    print('Your discount: 0%')
-    print(f'Price is {user}')
-elif user >= 1000 and user < 5000:
-    print('Your discount: 5%')
-    print(f'Price is {user * 0.95}')
-elif user >= 5000 and user <10000:
-    print(f'Your discount: 10%')
-    print(f'Price is {user * 0.9}')
-elif user > 10000:
-    print('Your discount: 15%')
-    print(f'Price is {user * 0.85}')
-else:
-    print('Error: What is this suppose to mean?')
+    if user < 0:
+        print('Buy cost cant be negative')
+    elif 0 <= user < 1000:
+        print('Your discount: 0%')
+        print(f'Price is {user}')
+    elif user >= 1000 and user < 5000:
+        print('Your discount: 5%')
+        print(f'Price is {user * 0.95}')
+    elif user >= 5000 and user <10000:
+        print(f'Your discount: 10%')
+        print(f'Price is {user * 0.9}')
+    elif user > 10000:
+        print('Your discount: 15%')
+        print(f'Price is {user * 0.85}')
+except ValueError:
+    print('Allowed is numeric value, any other will be considered an error')
