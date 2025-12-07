3d0989e14c8be2422ace253f1a1cf681b4076790
idffs <235218353+idffs@users.noreply.github.com>
2025-12-08 01:58:11 +1000

    Create lab6_1.py

---
+++ Labs/lab6/lab6_1.py
@@ -0,0 +1,40 @@
+print('Works with ms, sec, min, h (milliseconds, seconds, minutes, hours)')
+ms = 'ms'
+sec = 'sec'
+m = 'min'
+h = 'h'
+def trans(a, b, c):
+    int(a)
+    if b == ms:
+        a = a
+    elif b == sec:
+        a = a * 1000
+    elif b == m:
+        a = a * 60 * 1000
+    elif b == h:
+        a = a * 60 * 60 * 1000
+    
+    if c == ms:
+        a = a
+    elif c == sec:
+        a = a / 1000
+    elif c == m:
+        a = a / 1000 / 60
+    elif c == h:
+        a = a / 1000 / 60 / 60
+    return a
+try:
+    a = float(input('Enter Value:'))
+except ValueError:
+    print('Invalid Input(Allowed - 15, 13.5, etc. )')
+    exit()
+try:
+    b = str(input('Enter Time Value that into transform:'))
+    c = str(input('Enter Time Value that in end transform:'))
+except ValueError:
+    print('Invalid Input(Allowed - ms, sec, m, h)')
+    exit()
+print(f'Entered data:{a} {b}')
+
+print('Output data:')
+print(f'{trans(a, b, c):.1f} {c}')
