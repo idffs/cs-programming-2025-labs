63653c046282f04766ccac64a322bbd1502ae13b
idffs <235218353+idffs@users.noreply.github.com>
2025-12-15 21:23:09 +1000

    Update lab4_4.py

--- Labs/lab4/lab4_4.py
+++ Labs/lab4/lab4_4.py
@@ -1,5 +1,11 @@
 try:
     user = input('Type number to know will it divisible by 6 or not: ')
+    if int(user) < 0:
+        print('Error: Type positive value')
+        exit()
+    elif int(user) == 0:
+        print('0 can\'t be divisible by 6')
+        exit()
     q = len(str(user))
     w = 0
     for i in range(q):
@@ -7,6 +13,6 @@ try:
     if int(user[-1]) % 2 == 0 and w % 3 == 0:
         print('It would divisible by 6')
     else:
-        print('This number wont divisible by 6')
+        print('This number won\'t divisible by 6')
 except ValueError:
     print('Error: Needs numeric value')
