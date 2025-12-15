7b5e38b75217a6529b102874c90d6cc5e06f12e7
idffs <235218353+idffs@users.noreply.github.com>
2025-12-15 21:23:08 +1000

    Update lab4_6.py

--- Labs/lab4/lab4_6.py
+++ Labs/lab4/lab4_6.py
@@ -1,10 +1,15 @@
 try:
-    user = int(input('Type a year(example, 2025): '))
-    if user % 4 == 0 and user % 100 != 0:
+    user = input('Type a year(example, 2025): ')
+    if '.' in user or ',' in user:
+        print('Error: Type a year without float \"."')
+        exit()
+    if int(user) < 0:
+        print('Error: Year can\'t be negative')
+        exit()
+    user = int(user)
+    if user % 4 == 0 and user % 100 != 0 or user % 400 == 0:
         print(f'{user} is leap year ')
-    elif user % 400 == 0:
-        print(f'{user} is leap year')
     else:
-        print(f'{user} isnt leap year')
+        print(f'{user} isn\'t leap year')
 except ValueError:
-    print('Error: Type a year in numeric value, not a word or smth else')
+    print('Error: Type a year in valid integer')
