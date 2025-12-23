4771cd442024b9f89e0b5504a99cdca0612ff990
idffs <235218353+idffs@users.noreply.github.com>
2025-12-23 11:59:32 +1000

    Update lab5_5.py

--- Labs/lab5/lab5_5.py
+++ Labs/lab5/lab5_5.py
@@ -1,5 +1,6 @@
-#Find max and min price of fruits(in task was anything from shop)
+#Find max and min price of fruits(in task was anything from market)
 dict1 = {'Bananas': 3, 'Apples': 3, 'Qiwi': 5, 'Cabbage': 2, 'Carrot': 3}
+print(dict1)
 ly = min(dict1, key=dict1.get)
 re = max(dict1, key=dict1.get)
 print(f'{re} - have max price, {ly} - have min price')
