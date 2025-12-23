ea09268ca2432ba0b585ee13310926a6d29d3076
idffs <235218353+idffs@users.noreply.github.com>
2025-12-23 11:59:35 +1000

    Update lab5_8.py

--- Labs/lab5/lab5_8.py
+++ Labs/lab5/lab5_8.py
@@ -12,7 +12,10 @@ Scissors decapitate a lizard
 The lizard eats paper
 Paper sets up Spock
 Spock vaporizes the stone
-The stone breaks the scissors''')
+The stone breaks the scissors
+
+To close programm enter "quit"
+I will remind it for you every 10 games''')
 item = ('stone', 'scissors', 'paper', 'lizard', 'spock')
 rules = {
     'stone': ['scissors', 'lizard'],
@@ -21,27 +24,45 @@ rules = {
     'lizard': ['spock', 'paper'],
     'spock': ['stone', 'scissors']
 }
-try:
-    print('''
-    1 - Stone
-    2 - Scissors
-    3 - Paper
-    4 - Lizard
-    5 - Spock
-    ''')
-    u= int(input('Enter number: '))
-    us = item[u-1]
-    if 1 <= u <= 5:
-        print(f'Selected: {us}')
+game = 0
+while True:
+    game += 1
+    if game % 10 == 0:
+        print('''
+        
+        Remind, to exit enter "quit"
+        
+        ''')
+    try:
+        print('''
+        1 - Stone
+        2 - Scissors
+        3 - Paper
+        4 - Lizard
+        5 - Spock
+        ''')
+        u= input('Enter number: ')
+        if u == 'quit':
+            print('Closing game...')
+            exit()
+        elif '.' in u or ',' in u:
+            print('Not use any float')
+            exit()
+        elif int(u) < 1 or int(u) > 5:
+            print('Error: Out of options')
+            exit()
+        else:
+            u = int(u)
+            us = item[u-1]
+            print(f'Selected: {us}')
+    except ValueError:
+        print('Error: Not number')
+        exit()
+    pc = random.choice(item)
+    print(f'{us} vs {pc}')
+    if us == pc:
+        print('Draw!')
+    elif pc in rules[us]:
+        print('User wins!')
     else:
-        exit(print('Out of options'))
-except ValueError:
-    exit(print('Not number'))
-pc = random.choice(item)
-print(f'{us} vs {pc}')
-if us == pc:
-    print('Draw!')
-elif pc in rules[us]:
-    print('User wins!')
-else:
-    print('PC wins!')
+        print('PC wins!')
