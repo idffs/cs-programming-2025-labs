f24326e9511d72562cb50bedde42c9ea6daa6864
idffs <235218353+idffs@users.noreply.github.com>
2026-01-13 12:09:46 +1000

    Create lab7_3.py

---
+++ Labs/lab7/lab7_3.py
@@ -0,0 +1,8 @@
+personnel = [
+    {"name": "Dr. Klein", "clearance": 2},
+    {"name": "Agent Brooks", "clearance": 4},
+    {"name": "Technician Reed", "clearance": 1}
+]
+
+res = list(map(lambda x: {'name': x['name'], 'clearance': x['clearance'], 'category': 'Restricted' if x['clearance'] == 1 else 'Confidential' if 2 <= x['clearance'] <= 3 else 'Top Secret' if x['clearance'] >= 4 else 'Broken Code'}, personnel))
+print(res)
