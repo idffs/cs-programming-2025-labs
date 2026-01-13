e1466c632dbd5737e0450d15116cd4aed9dc66c0
idffs <235218353+idffs@users.noreply.github.com>
2026-01-13 12:10:27 +1000

    Update lab6_3.py

--- Labs/lab6/lab6_3.py
+++ Labs/lab6/lab6_3.py
@@ -1,40 +1,35 @@
-def calcu(a, b):
-    if a // 10000 >= 16:
-        k1 = 5
+﻿def find_all_common(start, end):
+    if start > end:
+        return 'Start can\'t be more than end'
+    elif start < 1 or end < 1:
+        return 'Start or end can\'t be less than 1'
+    elif end == 1:
+        return 'End can\'t be 1'
+    
+    def common(c):
+        if c < 2:
+            return False
+        if c == 2:
+            return True
+        if c % 2 == 0:
+            return False
+        
+        for i in range(3, int(c**0.5) + 1, 2):
+            if c % i == 0:   
+                return False
+        return True
+    
+    commons = []
+    for com in range(start, end + 1):
+        if common(com):
+            commons.append(com)
+    
+    if commons:
+        return " ".join(map(str, commons))
     else:
-        k1 = 0.3 * (a // 10000)
-
-    if 1 <= b < 4:
-        k2 = 3
-    elif 4 <= b < 7:
-        k2 = 5
-    else: 
-        k2 = 2
-    
-    ks = k1 + k2
-    total_income = 0
-    current_amount = a
-    
-
-    for year in range(b):
-        year_income = current_amount * (ks / 100)
-        total_income += year_income
-        current_amount += year_income  
-    
-    return a, b, total_income, k1, k2
-
-
+        return 'Error'
 try:
-    a, b = map(int, input('Enter your deposit and length in integer (value years): ').split())
-    if a < 30000:
-        print('Minimal deposit is 30,000')
-    else:
-        initial_deposit, years, income, k1, k2 = calcu(a, b)
-        print(f"""
-        Coefficient 1 - {k1}
-        Coefficient 2 - {k2}
-        Income - {income:.2f}
-        Final amount - {initial_deposit + income:.2f}
-        """)
+    a, b = map(int, input('Enter radius(3 5, 1 17, 8 90, etc.): ').split())
+    print(find_all_common(a, b))
 except ValueError:
-    print('Only integers are allowed')
+    print('Start and end should be integers')
