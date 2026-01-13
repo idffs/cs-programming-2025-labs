a5ffdc7325d4baad859dbca7494a3b2f9acf5121
idffs <235218353+idffs@users.noreply.github.com>
2026-01-13 12:10:28 +1000

    Update lab6_2.py

--- Labs/lab6/lab6_2.py
+++ Labs/lab6/lab6_2.py
@@ -1,31 +1,43 @@
+print('Minimal deposit 30k(30000)')
 def calcu(a, b):
-    if a // 10000 >= 16:
+    if a // 10000 > 16:
         k1 = 5
     else:
         k1 = 0.3 * (a // 10000)
-    if b in range(1, 4):
+    if 1 <= b <= 3:
         k2 = 3
-    elif b in range(4, 7):
+    elif 4 <= b <= 6:
         k2 = 5
     elif b > 6:
         k2 = 2
     ks = k1 + k2
-    income = 0
+    c = 0
+    y = 0
+    ksf = ks / 100
+    a = a
     while y != b:
-        c += a * (ks / 100)
+        c += a * ksf
         y += 1
         a += c
-    return a, b, income, k1, k2
+    c = round(c)
+    return {
+        'cof1': k1, 
+        'cof2': k2,
+        'income':c
+    }
 try:
     a, b = map(int, input('Enter your deposit and length in integer(value years): ').split())
     if a < 30000:
-        print('Minimal deposit 30k(30000)')
+        print('Less than minimal deposit')
         exit()
-    calcu(a, b)
+    elif b < 1:
+        print('Deposit lenght can\'t be less 1 year')
+        exit()
+    res = calcu(a, b)
     print(f"""
-    coefficient 1 - {k1}
-    coefficient 2 - {k2}
-    income - {c}
+    coefficient 1 - {res['cof1']}
+    coefficient 2 - {res['cof2']}
+    income - {res['income']}
     """)
 except ValueError:
-    print('Not allowed any other symbols except integer numbers')
+    print('Not allowed any other symbols except integers')
