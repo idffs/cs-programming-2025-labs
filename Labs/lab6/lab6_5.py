12abeb51ee331e067833b53e64d3f0f28e3bef20
idffs <235218353+idffs@users.noreply.github.com>
2026-01-13 12:10:23 +1000

    Update lab6_5.py

---
+++ Labs/lab6/lab6_5.py
@@ -0,0 +1,8 @@
+﻿def check_for_palindrom(a):
+    clear = ''.join(char.lower() for char in a if char.isalnum())
+    if clear == clear[::-1]:  
+        return 'Yes, it\'s palindrom'
+    else:
+        return 'No, it\'s not palindrom'
+user = str(input('Enter your sentence: '))
+print(check_for_palindrom(user))
